1050fd7cff03e835c4a1f3d4a4d4e9b8495437a7
RizwanMushtaq <rizwanmushtaq15@gmail.com>
2021-11-02 07:28:30 +0100

    making it better

--- src/utils/logHandling.js
+++ src/utils/logHandling.js
@@ -1,3 +1,5 @@
 export function logWithDebug(message){
-    if(localStorage.getItem('Ottonova.debug')) console.log(message)
+    if(localStorage.getItem('Ottonova.debug') === true) {
+        console.log(message)
+    } 
 }
